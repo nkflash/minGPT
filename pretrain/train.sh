a8f1fd6f8aea55763bb6cfdade3f53febcfbc763
Xuebin Min <xbmin@baai.ac.cn>
2023-04-26 10:59:50 +0800

    add checkpoint train script

--- pretrain/train.sh
+++ pretrain/train.sh
@@ -2,5 +2,6 @@
 
 export OMP_NUM_THREADS=2
 
-torchrun --nproc_per_node=2 --nnodes=1 --node_rank=$RANK --rdzv_id=456 --rdzv_backend=c10d \
-  --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT pretrain.py --launch_type=ddp
+torchrun --nproc_per_node=1 --nnodes=2 --node_rank=$RANK --rdzv_id=456 --rdzv_backend=c10d \
+  --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT pretrain.py --launch_type=ddp --checkpoint_iters=400
+# --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT pretrain.py --launch_type=ddp --start_from_checkpoint
